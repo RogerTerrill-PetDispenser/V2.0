51fcdfcdb8f5cca08147cd9590ab74e8334329a0
Roger Dodger <lpaddikt@gmail.com>
2020-11-22 23:20:30 -0800

    Cleaned up the code.

--- AutoFeederV2.py
+++ AutoFeederV2.py
@@ -1,23 +1,11 @@
-# ************************************************************************** #
-# ****                                                                  **** #
-# *********** Code Designed by www.TheEngineeringProjects.com ************** #
-# ****                                                                  **** #
-# ****************** How to Create a GUI in Raspberry Pi 3 ***************** #
-# ****                                                                  **** #
-# ************************************************************************** #
-
-# Importing Libraries
-
 import RPi.GPIO as GPIO
 import threading
 import time
 import schedule
-from datetime import date
 from guizero import App, Text, PushButton, Window
 
 # Raspberry Pi 3 Pin Settings
-
-LED = 11 # pin11
+LED = 11
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) # We are accessing GPIOs according to their physical location
 GPIO.setup(LED, GPIO.OUT) # We have set our LED pin mode to output
@@ -26,7 +14,8 @@ GPIO.output(LED, GPIO.LOW) # When it will start then LED will be OFF
 # Raspberry Pi 3 Pin Settings
 enabled = True
 led_text = "OFF"
-# Funtion for Buttons started here
+# Function for Buttons started here
+
 
 def manual_feed_toggle():
     if GPIO.input(11):
@@ -35,11 +24,13 @@ def manual_feed_toggle():
         GPIO.output(LED, GPIO.HIGH)
     print(GPIO.input(11))
 
+
 def pause():
     global enabled
     enabled = not enabled
     print("Disabled" if enabled else "Enabled")
-    
+
+
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
     job_thread.start()
@@ -48,11 +39,13 @@ def run_threaded(job_func):
 def manual_single_feed():
     print("Manual Feeding the BINKY!!!")
 
+
 def run_feeding_schedule():
     schedule.every().day.at("22:58").do(manual_single_feed)
     while True:
         schedule.run_pending()
         time.sleep(1)
+
 
 app = App(title="Binky Food Machine V2")
 manual_feed_toggle_button = PushButton(app, text="Toggle Feed", command=manual_feed_toggle, width="fill", height="fill")
