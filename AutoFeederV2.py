e8328e397fe5fefcc91c7ceb6af5a3c2ddd23a01
Roger Dodger <lpaddikt@gmail.com>
2020-11-29 19:38:19 -0800

    Added full layout and coloring for buttons

--- AutoFeederV2.py
+++ AutoFeederV2.py
@@ -3,7 +3,7 @@ import threading
 import time
 import schedule
 from datetime import datetime
-from guizero import App, Text, PushButton, Window
+from guizero import App, Text, PushButton, Window, Box
 
 # Raspberry Pi 3 Pin Settings
 LED = 11
@@ -17,6 +17,11 @@ enabled = True
 led_text = "OFF"
 food_delay = 8.100
 current_time = ""
+
+GREEN = "green"
+RED = "red"
+WHITE = "white"
+BLUE = "#06BEE1"
 # Function for Buttons started here
 
 
@@ -32,7 +37,7 @@ def pause():
     global enabled
     enabled = not enabled
     pause_feed_button.text = "Binky Food Machine Is Paused" if not enabled else "Binky Food Machine Is Enabled"
-    pause_feed_button.bg = "red" if not enabled else "green"
+    pause_feed_button.bg = RED if not enabled else GREEN
     if(not enabled):
         manual_single_feed_button.disable()
         manual_feed_toggle_button.disable()
@@ -52,8 +57,8 @@ def single_feed():
         GPIO.output(LED, GPIO.HIGH)
         manual_single_feed_button.disable()
         manual_feed_toggle_button.disable()
-        manual_feed_toggle_button.bg = "white"
-        manual_single_feed_button.bg = "white"
+        manual_feed_toggle_button.bg = WHITE
+        manual_single_feed_button.bg = WHITE
         print("Manual Feeding the BINKY!!!")
         
         time.sleep(food_delay)
@@ -61,8 +66,8 @@ def single_feed():
         GPIO.output(LED, GPIO.LOW)
         manual_single_feed_button.enable()
         manual_feed_toggle_button.enable()
-        manual_feed_toggle_button.bg = "green"
-        manual_single_feed_button.bg = "green"
+        manual_feed_toggle_button.bg = BLUE
+        manual_single_feed_button.bg = BLUE
         print("Done feeding")
     else:
         print("Sorry, binky machine is disabled")
@@ -89,19 +94,36 @@ def display_time():
 
 
 app = App(title="Binky Food Machine V2")
-header_text = Text(app, size=24)
-manual_feed_toggle_button = PushButton(app, text="Toggle Feed", command=manual_feed_toggle, width="fill", height="fill")
-manual_feed_toggle_button.bg = "green"
-manual_feed_toggle_button.text_color = "white"
+
+# Title box
+title_box_container = Box(app, width="fill", align="top", border=True)
+title_box_left = Box(title_box_container, width="fill", align="left")
+title_box_right = Box(title_box_container, width="fill", align="right")
+header_text = Text(title_box_left, size=24, height="fill", width="fill")
+last_fed_text = Text(title_box_right, size=24, align="top", height="fill", width="fill", text="Binky Last Fed")
+last_fed_tome = Text(title_box_right, size=24, align="bottom", height="fill", width="fill", text="--:--")
+
+# Button box
+buttons_box = Box(app, width="fill", height="fill", align="top", border=True)
+
+manual_feed_toggle_button = PushButton(buttons_box, align="left", text="Toggle Feed", width="fill", height="fill", command=manual_feed_toggle)
+manual_feed_toggle_button.bg = BLUE
+manual_feed_toggle_button.text_color = WHITE
 manual_feed_toggle_button.text_size = 16
-manual_single_feed_button = PushButton(app, text="Manual Feed", command=manual_single_feed, width="fill", height="fill")
-manual_single_feed_button.bg = "green"
-manual_single_feed_button.text_color = "white"
+
+manual_single_feed_button = PushButton(buttons_box, align="right", text="Manual Feed", width="fill", height="fill", command=manual_single_feed)
+manual_single_feed_button.bg = BLUE
+manual_single_feed_button.text_color = WHITE
 manual_single_feed_button.text_size = 16
-pause_feed_button = PushButton(app, text="Binky Food Machine Is Enabled", command=pause, width="fill", height="fill")
-pause_feed_button.bg = "green"
-pause_feed_button.text_color = "white"
+
+# Status box
+status_box = Box(app, width="fill", height="fill", align="bottom", border=True)
+
+pause_feed_button = PushButton(status_box, text="Binky Food Machine Is Enabled", width="fill", height="fill", command=pause)
+pause_feed_button.bg = GREEN
+pause_feed_button.text_color = WHITE
 pause_feed_button.text_size = 16
+
 run_threaded(run_feeding_schedule)
 run_threaded(display_time)
 app.display()
